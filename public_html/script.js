bf26b7fb831beb1e08664382e3fd2e66ebe8587c
Brandon Adonis Ortez <phoenixflower111@hotmail.com>
2015-01-14 14:54:38 -0800

    hi
    by

--- public_html/script.js
+++ public_html/script.js
@@ -8,56 +8,67 @@
 $('document').ready(function(){
     $('.yeah').css('background-color', 'yellow');
   
-    $('div:last p:nth-child(3)').css('background-color', 'pink');
-    
-    $('p:last').css('color', 'red');
-    
-    $('p:gt(1)').css('border-style', 'solid');
-    
-    $('p:first').css({'background-color':'cyan', 'color':'white'});
-    
-    $('.2').css({'visibility': 'hidden', 'position': 'absolute'});
-    
-    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-    
-    $('h1').bind('click', mouseClick);
-    
-    $('#replaceWText').css('color', 'blue');
-    
-    $('#replaceWText').bind('click', replaceWText);
-    
-    $('#randPara').bind('click', addAPara);
-    
-    $('#removePara').bind('click', removeAPara);
-    
-    $('#highlightlogo').bind('click', highlightlogo);
+//    $('div:last p:nth-child(3)').css('background-color', 'pink');
+//    
+//    $('p:last').css('color', 'red');
+//    
+//    $('p:gt(1)').css('border-style', 'solid');
+//    
+//    $('p:first').css({'background-color':'cyan', 'color':'white'});
+//    
+//    $('.2').css({'visibility': 'hidden', 'position': 'absolute'});
+//    
+//    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+//    
+//    $('h1').bind('click', mouseClick);
+//    
+//    $('#replaceWText').css('color', 'blue');
+//    
+//    $('#replaceWText').bind('click', replaceWText);
+//    
+//    $('#randPara').bind('click', addAPara);
+//    
+//    $('#removePara').bind('click', removeAPara);
+
+      $('#show').css('visibility', 'hidden');
+      
+      $('#hide').bind('click', hideThePage);
+      
+      $('#show').bind('click', showThePage);
 });
 
-function highlightlogo(){
-    $('h1').effect('highlight', {color: 'red'}, 500);
+function hideThePage(){
+    $('#show').css('visibility', 'visible');
+    $('div').hide('slide', {}, 2500);
+    $('#show').show('fold', {}, 2500);
 }
 
-function removeAPara(){
-    $('#randPara p:last').remove();
+function showThePage(){
+    $('div').show('fold', {}, 2500);
+    $('#show').hide('puff', {}, 2500);
 }
 
-function addAPara(){
-    $('#randPara').append('<p>ADDED</p>');
-}
-
-function replaceWText(){
-    $('#replaceWText').text('<h1>Replaced!</h1>');
-}
-
-function mouseOverMe(){
-    $("h1").html("Hi");
-}
-
-function mouseOutMe(){
-    $('h1').html('Bye');
-}
-
-function mouseClick(){
-    
-    $('p').html('Hi and bye 101.');
-}
+//function removeAPara(){
+//    $('#randPara p:last').remove();
+//}
+//
+//function addAPara(){
+//    $('#randPara').append('<p>ADDED</p>');
+//}
+//
+//function replaceWText(){
+//    $('#replaceWText').text('<h1>Replaced!</h1>');
+//}
+//
+//function mouseOverMe(){
+//    $("h1").html("Hi");
+//}
+//
+//function mouseOutMe(){
+//    $('h1').html('Bye');
+//}
+//
+//function mouseClick(){
+//    
+//    $('p').html('Hi and bye 101.');
+//}
